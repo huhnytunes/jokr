033d8ddb1f4083fe4c103651ca229bb8e410ca38
DineshRai <drai89@gmail.com>
2014-05-29 17:36:00 -0400

    new reddit

--- reddit.rb
+++ reddit.rb
@@ -1,15 +1,19 @@
 require "ruby_reddit_api"
 require "launchy"
 
+
+
 r = Reddit::Api.new "user", "password"
 
-# results = r.browse "aww"
-results = r.browse "jokes"
+resuts = r.browse "aww"
+# results = r.browse "jokes"
 
 
 index = rand(0..24)
-p results[index.to_i].title
-sleep 3
-p results[index.to_i].selftext
+p results[index.to_i].thumbnail
+# sleep 3
+# p results[index.to_i].selftext
 # Launchy.open("reddit.com" << results[index.to_i].permalink.to_s)
+
+Launchy.open(results[index.to_i].url)
 
