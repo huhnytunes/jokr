ed257150a8b0afe4f71ea2b5fd2de01b28334efa
DineshRai <drai89@gmail.com>
2014-05-30 10:58:42 -0400

    joker gme format

---
+++ lib/viewer.rb
@@ -0,0 +1,48 @@
+require "launchy"
+
+class Viewer
+
+  def welcome_screen
+    puts 
+    puts "Welcome to Jokr. Get ready to laugh." 
+    puts 
+  end 
+
+  def choice_screen
+    puts "What would you like to see?" 
+    puts 
+    puts "1 for a Joke"
+    puts "2 for a Funny Picture" 
+    gets.chomp 
+  end 
+
+  def display_joke(joke)
+    puts 
+    puts joke
+    puts 
+    sleep(3)
+  end 
+
+  def display_image(punchline, image)
+    puts 
+    puts punchline
+    sleep(3)
+    Launchy.open(image)
+  end 
+
+  def display_punch_line(punch_line)
+    puts punch_line
+    see_another?
+  end 
+
+  def see_another?
+    puts 
+    puts "Would you like to laugh again ?"
+    gets.chomp
+  end 
+
+  def good_bye
+    puts "Come back soon !!"
+  end 
+
+end 
